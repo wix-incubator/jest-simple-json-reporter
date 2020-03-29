6a27adc5f4361bd98398564faa48d7e695d271f7
stavalfi <stavalfi@gmail.com>
2020-03-30 01:14:01 +0300

    deep-sort - fix types again

--- packages/deep-sort/src/index.ts
+++ packages/deep-sort/src/index.ts
@@ -6,10 +6,7 @@ type Ref = Literal | ObjectRef | (Literal | ObjectRef)[]
 
 export default function deepSort<T extends Ref>(ref: T): T {
   if (Array.isArray(ref)) {
-    const hashToCell: ObjectRef = ref.reduce<ObjectRef>(
-      (acc, cell) => Object.assign({}, acc, { [toHash(cell)]: cell }),
-      {},
-    )
+    const hashToCell: ObjectRef = ref.reduce<ObjectRef>((acc, cell) => ({ ...acc, [toHash(cell)]: cell }), {})
     const sortedArray = ref
       .map<string>(cell => toHash(cell))
       .sort()
@@ -21,10 +18,11 @@ export default function deepSort<T extends Ref>(ref: T): T {
   if (typeof ref === 'object') {
     const objWithSortedValues = Object.entries(ref)
       .map<[string, Ref]>(([key, value]) => [key, deepSort(value)])
-      .reduce<Ref>((acc, [key, value]) => Object.assign({}, acc, { [key]: value }), {})
+      .reduce<ObjectRef>((acc, [key, value]) => ({ ...acc, [key]: value }), {})
+
     const sortedObj = Object.keys(objWithSortedValues)
       .sort()
-      .reduce<Ref>((acc, key) => Object.assign({}, acc, { [key]: objWithSortedValues[key] }), {})
+      .reduce<ObjectRef>((acc, key) => ({ ...acc, [key]: objWithSortedValues[key] }), {})
 
     return sortedObj as T
   }
